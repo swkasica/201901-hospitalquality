d2ee0437e5a6c43ab38526e9887e46bdc0b554c3
MaryJo Webster <mjwebster71@gmail.com>
2019-01-10 15:44:26 -0600

    change readmission variables

--- hospital_quality_script.R
+++ hospital_quality_script.R
@@ -11,6 +11,12 @@ survey <- read_csv("Patient_survey_HCAHPS_Hospital.csv") %>% filter(State=='MN')
 
 
 
+readmission2 <- readmission %>% mutate(discharges=case_when(`Number of Discharges`=='Not Available'~0,
+                                                            TRUE~as.numeric(`Number of Discharges`)))
+
+
+
+
 #infections table has one record per hospital
 #winnow down to just the fields we care about
 infections_2 <-  infections %>% select(Hospital_Name, `Provider ID`,Total_HAC_Score, Total_HAC_Footnote) %>% 
@@ -18,10 +24,15 @@ infections_2 <-  infections %>% select(Hospital_Name, `Provider ID`,Total_HAC_Sc
 
 
 
-#pull out readmission data for heart attacks
-readmission_ami <-  readmission %>% filter(`Measure Name`=='READM-30-AMI-HRRP') %>% select(`Hospital Name`, `Provider Number`, 
+#pull out readmission data for heart failure
+readmission_hf<-  readmission %>% filter(`Measure Name`=='READM-30-HF-HRRP') %>% select(`Hospital Name`, `Provider Number`, 
                                                                                            `Excess Readmission Ratio`) %>% 
-  rename(AMI_readmission=`Excess Readmission Ratio`, hospitalID=`Provider Number`, hospital=`Hospital Name`)
+  rename(HF_readmission=`Excess Readmission Ratio`, hospitalID=`Provider Number`, hospital=`Hospital Name`)
+
+#readmission rates for pneumonia
+readmission_pn<-  readmission %>% filter(`Measure Name`=='READM-30-PN-HRRP') %>% select(`Hospital Name`, `Provider Number`, 
+                                                                                        `Excess Readmission Ratio`) %>% 
+  rename(PN_readmission=`Excess Readmission Ratio`, hospitalID=`Provider Number`, hospital=`Hospital Name`)
 
 
 #pull out readmission data for hip/knee surgeries
@@ -44,9 +55,11 @@ star_rating <-  survey %>% filter(`HCAHPS Measure ID`=='H_STAR_RATING') %>%
 
 hospitals <- inner_join(star_rating, infections_2 %>% select(hospitalID, Total_HAC_Score, Total_HAC_Footnote) , by=c("hospitalID"="hospitalID"))
 
-hospitals <-  inner_join(hospitals, readmission_ami %>% select(hospitalID, AMI_readmission), by=c("hospitalID"="hospitalID"))    
+hospitals <-  inner_join(hospitals, readmission_hf %>% select(hospitalID, HF_readmission), by=c("hospitalID"="hospitalID"))    
 
 hospitals <-  inner_join(hospitals, readmission_hip_knee %>% select(hospitalID, HIP_KNEE_readmission), by=c("hospitalID"="hospitalID"))   
+
+hospitals <-  inner_join(hospitals, readmission_pn %>% select(hospitalID, PN_readmission), by=c("hospitalID"="hospitalID"))   
 
 
 write.csv(hospitals, "hospitals_fordatawrapper.csv", row.names=FALSE)
